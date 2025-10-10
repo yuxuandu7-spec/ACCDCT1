fda08a356fb67a1c7df03d8a9d75bbf04bd711bd
yuxuandu7-spec <yuxuandu7@gmail.com>
2025-10-10 11:59:00 -0700

    1

---
+++ week4/inclass/sketch.js
@@ -0,0 +1,45 @@
+let posX
+let posY
+
+let velX
+let velY
+
+let radius = 20
+
+function setup() {
+  createCanvas(600, 600);
+
+  colorMode(HSB, width, 100, 100)
+  posX = width * 0.5
+  posY = height * 0.5
+
+  velX = -2
+  velY = 3 
+}
+
+function draw() {
+  posX = posX + velX
+  posY += velY
+
+  if(posY  + radius >= height || posY - radius <=0){
+    velY = velY *-1
+  }
+
+  if(posX + radius >= width || posX - radius <=0){
+    velX = velX *-1
+  }
+
+ background(0, 0, 85)
+
+ noStroke()
+
+ fill(posX, 100, 100)
+
+ circle(posX, posY, radius * 2)
+
+ stroke(255, 0, 0)
+ strokeWeight(4)
+
+ fill(width * 0.75, 100, 100)
+ rect(width * 0.5 - 50, height * 0.5 - 50, 100, 100)
+}
